c559ac10805dce7bb6e3cac22d2528f2271cff3b
Dimas <bonifasiuspranowo@gmail.com>
2026-04-15 20:33:51 +0700

    Change Top Margin to 5

--- HMI_RASPI/main.py
+++ HMI_RASPI/main.py
@@ -846,7 +846,7 @@ def main(page: ft.Page):
         # Pastikan tombol btn_exit ini masuk ke dalam Column daftar controls
         layout = build_standard_layout(
             ft.Column([
-                ft.Container(content=ft.Text("Smart Drawer System", size=40, weight="bold", color=TEXT_COLOR), margin=ft.margin.only(top=20)),
+                ft.Container(content=ft.Text("Smart Drawer System", size=40, weight="bold", color=TEXT_COLOR), margin=ft.margin.only(top=5)),
                 ft.Row([
                     create_menu_card("Admin", "Kelola sistem", "admin.png", "#E3F2FD", lambda _: show_rfid_page("Scan Kartu Admin", show_login_admin, show_home, "admin")),
                     create_menu_card("User", "Pinjam alat", "user.png", "#E8F5E9", lambda _: show_menu_user())
