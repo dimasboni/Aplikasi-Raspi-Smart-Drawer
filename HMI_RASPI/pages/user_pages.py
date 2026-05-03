bee88fd230d1b2ddc719fae0a2525d5aba8ec518
dimasboni <bonifasiuspranowo@gmail.com>
2026-05-03 19:44:42 +0700

    Adjusting margin for menu user

--- HMI_RASPI/pages/user_pages.py
+++ HMI_RASPI/pages/user_pages.py
@@ -67,7 +67,7 @@ def register_user_pages(page: ft.Page, session_data: dict, nav: dict):
                     ],
                     horizontal_alignment="center",
                     alignment="center",
-                    margin=ft.margin.only(top=-100)
+                    margin=ft.margin.only(top=-150)
                 ),
                 back_func=nav["show_home"],
             )
