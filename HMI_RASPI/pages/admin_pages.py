25df0c7bc7cebd08b95b1a62ab9cfca1be5c76bd
Dimas <bonifasiuspranowo@gmail.com>
2026-06-30 20:24:14 +0700

    Mengubah tinggi add tool method jadi -100 untuk margin topnya

--- HMI_RASPI/pages/admin_pages.py
+++ HMI_RASPI/pages/admin_pages.py
@@ -132,7 +132,7 @@ def register_admin_pages(page: ft.Page, session_data: dict, nav: dict):
                     ],
                     horizontal_alignment="center",
                     alignment="center",
-                    margin=ft.margin.only(top=-50),
+                    margin=ft.margin.only(top=-100),
                 ),
                 back_func=show_edit_tools_menu, # Kembali ke menu utama Edit Tools
             )
